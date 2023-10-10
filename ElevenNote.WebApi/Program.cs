d4cfb7883813dafb8a6feaeb25ddaf413005fc58
Martin Flaherty <mflaherty@elevenfifty.org>
2023-10-10 11:34:10 -0400

    IUserService and UserService initial setup and DI config

--- ElevenNote.WebApi/Program.cs
+++ ElevenNote.WebApi/Program.cs
@@ -1,11 +1,13 @@
 using ElevenNote.Data;
 using ElevenNote.Data.Entities;
+using ElevenNote.Services.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUserService, UserService>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
@@ -20,7 +22,7 @@ builder.Services.AddDefaultIdentity<UserEntity>(options =>
 })
     .AddRoles<IdentityRole<int>>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-    
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
