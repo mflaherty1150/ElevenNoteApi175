f9a4a1e41fa64290959529e6f8f1a5cf2ab62b17
Martin Flaherty <mflaherty@elevenfifty.org>
2023-10-10 12:24:53 -0400

    UserController initial scaffolding

---
+++ ElevenNote.WebApi/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+using ElevenNote.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElevenNote.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+}
+
