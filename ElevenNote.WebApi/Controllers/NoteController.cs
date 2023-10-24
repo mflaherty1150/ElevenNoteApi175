7bac7d11d24c9574530e6566baafc74129e9936c
Martin Flaherty <mflaherty@elevenfifty.org>
2023-10-24 11:02:59 -0400

    Added NoteController scaffolding (with Authorization)

---
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,19 @@
+using ElevenNote.Services.Note;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElevenNote.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+    }
+}
